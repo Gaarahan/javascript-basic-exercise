ebd927737a6848792a871529e70ec6b402cac9ae
gaarahan <gaarahan@foxmail.com>
2020-07-18 20:37:27 +0800

    [Gaarahan] implement arrayToList.js

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,27 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Creating list from undefined array')
+  }
+  if (!array.length) {
+    throw new Error('Creating list from empty array')
+  }
+
+  function Node(val) {
+    this.value = val;
+    this.next = null;
+  }
+  let head, pre, cur;
+  array.forEach((itm, i) => {
+    if (i === 0) {
+      head = new Node(itm);
+      pre = head;
+    } else {
+      cur = new Node(itm);
+      pre.next = cur;
+      pre = cur;
+    }
+  })
+  return head;
 }
