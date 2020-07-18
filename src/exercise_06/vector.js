2ad9dea981fe68d409861b0fbb3cd8e63dc469ea
gaarahan <gaarahan@foxmail.com>
2020-07-18 20:38:01 +0800

    [Gaarahan] implement vector.js

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -12,5 +12,29 @@ export default class Vector {
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  distance() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+  }
+
+  static plus (a, b) {
+    return new Vector(a.x + b.x, a.y + b.y);
+  }
+
+  static minus (a, b) {
+    return new Vector(a.x - b.x, a.y - b.y);
+  }
+
 }
